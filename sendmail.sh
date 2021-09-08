6edf5a8175fedfd851674b3076b27f1e4a7c2ee4
Anurag Shrivastava <anurag939@gmail.com>
2021-09-08 11:04:03 +0300

    Update sendmail.sh

--- sendmail.sh
+++ sendmail.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
+
+(
+TO_LIST=anuragshrivastava18@gmail.com
+CC_LIST=anurag939@gmail.com
+BCC_LIST=jmphoenix27@gmail.com
+MSG_SUB='Are you coming to school today?'
+MSG_BODY='Hello this is test email , ignore it'
+echo "MIME-Version: 1.0
+From: anurag@hbku.edu.qa
+To: $TO_LIST
+Cc: $CC_LIST
+Bcc: $BCC_LIST
+Subject: $MSG_SUB
+Content-Type: text/html
+$MSG_BODY
+"
+) | /usr/sbin/sendmail  -t
 
