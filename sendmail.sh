29e4439e36b11ea86dec152f0208988d159760f8
Anurag Shrivastava <anurag939@gmail.com>
2022-10-18 11:41:30 +0300

    a s c

--- sendmail.sh
+++ sendmail.sh
@@ -5,7 +5,7 @@ TO_LIST=anuragshrivastava18@gmail.com
 CC_LIST=anurag939@gmail.com
 BCC_LIST=jmphoenix27@gmail.com
 MSG_SUB='Are you coming to school today?'
-MSG_BODY='Hello this is test email , ignore it'
+MSG_BODY='Hello this is just test email , ignore it'
 echo "MIME-Version: 1.0
 From: anurag@hbku.edu.qa
 To: $TO_LIST
